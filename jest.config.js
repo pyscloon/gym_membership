333e1205e5209dbfc5c296e25865871dc92af87a
pyscloon <zivbarcelona13@gmail.com>
2026-04-19 20:33:58 +0800

    Revert "chore: removed unnecessary test"
    This reverts commit 113172c3489543ea2fbb66e229389c85179652d6.

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,8 @@ export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/**/*.test.ts'],
-  setupFiles: ['dotenv/config'],
   transform: {
-    '^.+\\.ts$': ['ts-jest', {
+    '^.+\.ts$': ['ts-jest', {
       tsconfig: 'tsconfig.test.json',
     }],
   },
